412d3c640fb3f385edc05cf39e8649744327545d
tomptrs <tom_ptrs@yahoo.com>
2013-12-08 20:54:25 +0100

    test

--- twitter-oauth/test.php
+++ twitter-oauth/test.php
@@ -1,6 +1,8 @@
-<?php session_start();
+<?php 
 
-echo "<title>Twitter oAuth Application by 1stwebdesigner | Login to your Twitter Account</title>";
+session_start();
+
+echo "Login to your Twitter Account";
 
 include 'lib/EpiCurl.php';
 include 'lib/EpiOAuth.php';
@@ -15,41 +17,14 @@ $oauth_token = $_GET['oauth_token'];
 echo $twitterOjb;
 echo $oauth_token;
 
-	if($oauth_token == '')
-  	  { 
+	
 	  	$url = $twitterObj->getAuthorizationUrl();
   		echo "<div style='width:200px;margin-top:200px;margin-left:auto;margin-right:auto'>";
         echo "<a href='$url'>Sign In with Twitter</a>";
         echo "</div>";
-     } 
-	else
-	  {
-		$twitterObj->setToken($_GET['oauth_token']);
-		$token = $twitterObj->getAccessToken();
-		$twitterObj->setToken($token->oauth_token, $token->oauth_token_secret);	  	
-		$_SESSION['ot'] = $token->oauth_token;
-		$_SESSION['ots'] = $token->oauth_token_secret;
-		$twitterInfo= $twitterObj->get_accountVerify_credentials();
-		$twitterInfo->response;
-		
-		$username = $twitterInfo->screen_name;
-		$profilepic = $twitterInfo->profile_image_url;
+    
 
-		include 'update.php';
-        
-     } 
-
-if(isset($_POST['submit']))
-	  {
-	  	$msg = $_REQUEST['tweet'];
-		
-		$twitterObj->setToken($_SESSION['ot'], $_SESSION['ots']);
-		$update_status = $twitterObj->post_statusesUpdate(array('status' => $msg));
-		$temp = $update_status->response;
-		
-		echo "<div align='center'>Updated your Timeline Successfully .</div>";
-		
-	  }
+	
 	  echo "<div style='margin-top:100px;'>";
 	  echo "<p>";
 	  echo "<center>";
