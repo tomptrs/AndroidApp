eaa59cd999909179ac995f3f1c4733fbfc9968cc
tomptrs <tom_ptrs@yahoo.com>
2013-12-08 20:47:56 +0100

    tmp

---
+++ twitter-oauth/test.php
@@ -0,0 +1,19 @@
+<?php session_start();
+
+echo "<title>Twitter oAuth Application by 1stwebdesigner | Login to your Twitter Account</title>";
+
+include 'lib/EpiCurl.php';
+include 'lib/EpiOAuth.php';
+include 'lib/EpiTwitter.php';
+include 'lib/secret.php';
+
+
+
+	  echo "<div style='margin-top:100px;'>";
+	  echo "<p>";
+	  echo "<center>";
+	  echo "<a href='http://www.1stwebdesigner.com/tutorials/how-to-update-twitter-using-php-and-twitter-api/'>Back to Tutorial</a>";
+	  echo "</center>";
+	  echo "</p>";
+	  echo "</div>";
+?> 
