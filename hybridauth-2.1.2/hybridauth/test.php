14f90126fb5dbbb317705de0a1c8b6dabf268dd1
tomptrs <tom_ptrs@yahoo.com>
2013-12-10 19:04:29 +0100

    hybridauth

---
+++ hybridauth-2.1.2/hybridauth/test.php
@@ -0,0 +1,18 @@
+<?php
+
+
+require_once( "Hybrid/Auth.php" );
+ $config   = dirname(__FILE__) . 'Hybrid/config.php';
+
+ 
+$hybridauth = new Hybrid_Auth( $config );
+
+// hybridauth will try to authenticate the user, then return an instance of Twitter adapter
+$twitter_adapter = $hybridauth->authenticate( "Twitter" );  
+ 
+// get the user profile 
+$twitter_user_profile = $twitter_adapter->getUserProfile();
+print $hybridauth;
+print $twitter_user_profile;
+
+?>
