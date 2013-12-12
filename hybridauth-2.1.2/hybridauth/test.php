be81429c6f8ca535a15050e113ce017295684285
tomptrs <tom_ptrs@yahoo.com>
2013-12-12 17:50:33 +0100

    test

--- hybridauth-2.1.2/hybridauth/test.php
+++ hybridauth-2.1.2/hybridauth/test.php
@@ -2,8 +2,8 @@
 	session_start(); 
 
 	// config and includes
-   	$config = dirname(__FILE__) . '/Hybrid/config.php';
-    require_once( "/Hybrid/Auth.php" );
+   //	$config = dirname(__FILE__) . '/Hybrid/config.php';
+    //require_once( "/Hybrid/Auth.php" );
 
 	print $config;
 	
