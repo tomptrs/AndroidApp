eb4e396f2d7f310b48f5d4ec095a448c6afce471
tomptrs <tom_ptrs@yahoo.com>
2013-12-12 17:59:37 +0100

    twitter test.php

--- hybridauth-2.1.2/hybridauth/test.php
+++ hybridauth-2.1.2/hybridauth/test.php
@@ -16,22 +16,22 @@
 		$hybridauth = new Hybrid_Auth( $config );
 
 		// automatically try to login with Twitter
-		//$twitter = $hybridauth->authenticate( "Twitter" );
+		$twitter = $hybridauth->authenticate( "Twitter" );
 
 		// return TRUE or False <= generally will be used to check if the user is connected to twitter before getting user profile, posting stuffs, etc..
-		//$is_user_logged_in = $twitter->isUserConnected();
+		$is_user_logged_in = $twitter->isUserConnected();
 
 		// get the user profile 
-		//$user_profile = $twitter->getUserProfile();
+		$user_profile = $twitter->getUserProfile();
 
 		// access user profile data
-		/*echo "Ohai there! U are connected with: <b>{$twitter->id}</b><br />";
+		echo "Ohai there! U are connected with: <b>{$twitter->id}</b><br />";
 		echo "As: <b>{$user_profile->displayName}</b><br />";
 		echo "And your provider user identifier is: <b>{$user_profile->identifier}</b><br />";  
 
 		// or even inspect it
 		echo "<pre>" . print_r( $user_profile, true ) . "</pre><br />";
-		*/
+		
 		}
 		catch( Exception $e ){  
 		// In case we have errors 6 or 7, then we have to use Hybrid_Provider_Adapter::logout() to 
