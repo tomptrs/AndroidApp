17656b83191d8218d10177935e755f84ec7e07c6
tomptrs <tom_ptrs@yahoo.com>
2013-12-27 13:30:58 +0100

    new authentication

--- Monitoring.php
+++ Monitoring.php
@@ -26,152 +26,17 @@ class Monitoring
 //			print  $line[1] . " " . $line[2] . "<br>";
 		}
 			//print $json[0];
-			return json_encode($json);
-		
+			return json_encode($json);	
 
 	}
 	
-	///
-	///
-	///
-	public function GetMonitoringPerPropertyData($propId,$agentId)
+	public function InsertUserInDB($name)
 	{
-		$sql = "SELECT Value as propName FROM t_monitoring m, t_agent a, t_properties p WHERE m.AgentId = a.Id and m.PropertyId = p.Id and p.Id = '$propId' and a.Id = '$agentId'  ";
-		$this->db->query($sql);
-		
-		
-		$teller = 1;
-		$json_data = array();
-		while ($line = $this->db->fetchArray() )
-		{
-		
-			array_push($json_data, $line[0]);		
-		
-		}
-		$json =  json_encode($json_data);
-		return $json;
+		//INSERT INTO `gocha`.`User` (`Name`, `ID`) VALUES (\'tom\', NULL);
+		$sql = "insert into User('Name','ID') VALUES ('{$name}',NULL);	
 	}
 	
 	
-	public function GetLatestMonitoringRequest($agentId)
-	{
-		$sql = "SELECT Max(`When`),`Value`, `Name`, p.Id FROM t_monitoring m, t_properties p WHERE m.PropertyId = p.Id group by p.Id  ";
-		
-		$this->db->query($sql);
-		
-		print " <div id='round' style='margin-top:50px;font-size:14px; '><p></p><table><tr><th>Property</th><th>Value</th><th>TimeStamp</th></tr>";
-		while ($line = $this->db->fetchArray() )
-		{
-			print  "<tr><td><a href='#' onclick='showDialog($line[3])'>" .$line[2] . "</a></td><td> " . $line[1] . "</td><td> " . $line[0]. "</td></tr>";
-		}
-		
-		print "</table> <p></p></div>";
-		
-	}
-	
-		public function GetLatestMonitoringRequestJSON($agentId)
-	{
-		$sql = "SELECT Max(`When`),`Value`, `Name`, p.Id FROM t_monitoring m, t_properties p WHERE m.PropertyId = p.Id group by p.Id  ";
-		
-		$this->db->query($sql);
-		
-		//print " <div id='round' style='margin-top:50px;font-size:14px; '><p></p><table><tr><th>Property</th><th>Value</th><th>TimeStamp</th></tr>";
-		// create a new XML document
-
-$rows = array();
-   
-   
-
-		while ($line = $this->db->fetchArray() )
-		{
-			//print  "<tr><td><a href='#' onclick='showDialog($line[3])'>" .$line[2] . "</a></td><td> " . $line[1] . "</td><td> " . $line[0]. "</td></tr>";
-			//$rows[] = $line[2];
-			 $rows[$line[2]][] =  $line[1];
-		}
-		
-//		print "</table> <p></p></div>";
-		return json_encode($rows);	
-	}
-	
-	public function GetLatestMonitoringRequestXML($agentId)
-	{
-		$sql = "SELECT Max(`When`),`Value`, `Name`, p.Id FROM t_monitoring m, t_properties p WHERE m.PropertyId = p.Id group by p.Id  ";
-		
-		$this->db->query($sql);
-		
-		//print " <div id='round' style='margin-top:50px;font-size:14px; '><p></p><table><tr><th>Property</th><th>Value</th><th>TimeStamp</th></tr>";
-		// create a new XML document
-
-			
-			
-
-	header("Content-Type: text/plain");
-
- 
-
-//create the xml document
-
-$xmlDoc = new DOMDocument();
-
-//create the root element
-
-$root = $xmlDoc->appendChild(
-
-$xmlDoc->createElement("root"));
-
-
-
-		
-		while ($line = $this->db->fetchArray() )
-		{
-	
-	
-   			
-			$tutTag = $root->appendChild(
-			
-				$xmlDoc->createElement("property"));
-   
-   
-			//create the author attribute
-			$p = (string)$line[2];
-			$v = (string)$line[1];
-			$t = (string)$line[0];
-			$tutTag->appendChild(
-
-					$xmlDoc->createAttribute("name"))->appendChild(
-
-					$xmlDoc->createTextNode($p) );
-			
-			$tutTag->appendChild(
-
-					$xmlDoc->createAttribute("value"))->appendChild(
-
-					$xmlDoc->createTextNode($v) );
-			
-			$tutTag->appendChild(
-
-					$xmlDoc->createAttribute("time"))->appendChild(
-
-					$xmlDoc->createTextNode($t) );
-  
-		}
-		
-		
-		
-
-
-
-//create a tutorial element
-
-
-//make the output pretty
-
-$xmlDoc->formatOutput = true;
-
-
-
-echo $xmlDoc->saveXML();
-	}
 	
 
 }
---
+++ auth.php
@@ -0,0 +1,53 @@
+<?php
+	session_start(); 
+	header('Access-Control-Allow-Origin: *');
+	// config and includes
+   	$config = dirname(__FILE__) . '/hybridauth-2.1.2/hybridauth/config.php';
+    require_once( "/hybridauth-2.1.2/hybridauth/Hybrid/Auth.php" );
+	
+	
+	try{
+		// hybridauth EP
+
+		$hybridauth = new Hybrid_Auth( $config );
+
+		// automatically try to login with Twitter
+		$twitter = $hybridauth->authenticate( "Twitter" );
+
+		// return TRUE or False <= generally will be used to check if the user is connected to twitter before getting user profile, posting stuffs, etc..
+		$is_user_logged_in = $twitter->isUserConnected();
+
+		// get the user profile 
+		$user_profile = $twitter->getUserProfile();
+
+		// access user profile data
+		echo "Ohai there! U are connected with: <b>{$twitter->id}</b><br />";
+		echo "As: <b>{$user_profile->displayName}</b><br />";
+		echo "And your provider user identifier is: <b>{$user_profile->identifier}</b><br />";  
+
+		/*
+		INSERT THE DISPLAYNAME IN MY DATABASE GOCHA
+		*/
+		
+		
+		// or even inspect it
+		echo "<pre>" . print_r( $user_profile, true ) . "</pre><br />";
+		 // grab the user's friends list
+		$user_contacts = $twitter->getUserContacts();
+		print $user_contacts;
+		print "try to get contacts";
+		// iterate over the user friends list
+			foreach( $user_contacts as $contact ){
+				echo $contact->displayName . " " . $contact->profileURL ." " . "<img src='". $contact->photoURL ."'> " ."<hr />";
+			}
+		}
+		catch( Exception $e ){  
+		// In case we have errors 6 or 7, then we have to use Hybrid_Provider_Adapter::logout() to 
+		// let hybridauth forget all about the user so we can try to authenticate again.
+
+		// Display the recived error, 
+		// to know more please refer to Exceptions handling section on the userguide
+			print "exception " . $e->getCode();
+		} 
+	
+	?>
