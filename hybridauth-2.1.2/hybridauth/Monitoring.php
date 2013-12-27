6214bc4f875a77273b946820faae816e0245c14d
tomptrs <tom_ptrs@yahoo.com>
2013-12-27 22:56:41 +0100

    test.php ; insert statement

--- hybridauth-2.1.2/hybridauth/Monitoring.php
+++ hybridauth-2.1.2/hybridauth/Monitoring.php
@@ -33,7 +33,7 @@ class Monitoring
 	public function InsertUserInDB($name)
 	{
 		//INSERT INTO `gocha`.`User` (`Name`, `ID`) VALUES (\'tom\', NULL);
-		$sql = "insert into User('Name','ID') VALUES (' ". $name . "',NULL);	
+		$sql = "INSERT INTO User (Name, ID) VALUES ('hihi', NULL);";	
 		$this->db->query($sql);	
 	}
 	
--- hybridauth-2.1.2/hybridauth/test.php
+++ hybridauth-2.1.2/hybridauth/test.php
@@ -23,9 +23,7 @@
 		// access user profile data
 		echo "Ohai there! U are connected with: <b>{$twitter->id}</b><br />";
 		echo "As: <b>{$user_profile->displayName}</b><br />";
-		echo "And your provider user identifier is: <b>{$user_profile->identifier}</b><br />";  
-
-		
+		echo "And your provider user identifier is: <b>{$user_profile->identifier}</b><br />";  		
 		
 		// or even inspect it
 		echo "<pre>" . print_r( $user_profile, true ) . "</pre><br />";
@@ -38,6 +36,15 @@
 				echo $contact->displayName . " " . $contact->profileURL ." " . "<img src='". $contact->photoURL ."'> " ."<hr />";
 			}
 		}
+		
+		/*
+		INSERT THE DISPLAYNAME IN MY DATABASE GOCHA
+		*/
+		require "Monitoring.php";
+		$m = new Monitoring();			
+			
+
+		$str = $m->InsertUserInDB($user_profile->displayName);
 		catch( Exception $e ){  
 		// In case we have errors 6 or 7, then we have to use Hybrid_Provider_Adapter::logout() to 
 		// let hybridauth forget all about the user so we can try to authenticate again.
